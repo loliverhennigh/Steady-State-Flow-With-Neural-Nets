329feb8e9e8e2f475bef8c290bc997e9b83c6bd1
oliver <loliverhennigh101@gmail.com>
2017-03-27 18:00:01 -0600

    updated test a bit

--- test/flow_speed_test.py
+++ test/flow_speed_test.py
@@ -35,10 +35,6 @@ tf.app.flags.DEFINE_float('keep_prob', 0.7,
                             """ keep probability for dropout """)
 tf.app.flags.DEFINE_float('learning_rate', 1e-5,
                             """ keep probability for dropout """)
-tf.app.flags.DEFINE_bool('display_test', True,
-                            """ display the test images """)
-tf.app.flags.DEFINE_string('test_set', "car",
-                            """ either car or random """)
 
 TEST_DIR = make_checkpoint_path(FLAGS.base_dir, FLAGS)
 
@@ -60,15 +56,11 @@ def evaluate():
     top_k_op: Top K op.
     summary_op: Summary op.
   """
-  # get a list of image filenames
-  filenames = glb('/data/fluid_flow_steady_state_128x128_test/*')
-  filenames.sort(key=alphanum_key)
-  filename_len = len(filenames)
   shape = [128, 256]
 
   with tf.Graph().as_default():
     # Make image placeholder
-    boundary_op = tf.placeholder(tf.float32, [1, shape[0], shape[1], 1])
+    boundary_op = tf.placeholder(tf.float32, [8, shape[0], shape[1], 1])
 
     # Build a Graph that computes the logits predictions from the
     # inference model.
@@ -87,28 +79,18 @@ def evaluate():
     
     graph_def = tf.get_default_graph().as_graph_def(add_shapes=True)
 
-    for run in filenames:
-      # read in boundary
-      flow_name = run + '/fluid_flow_0002.h5'
-      boundary_np = load_boundary(flow_name, shape).reshape([1, shape[0], shape[1], 1])
-      sflow_true = load_flow(flow_name, shape)
- 
+    num_runs = 1000
+    boundary_np = np.zeros([8, shape[0], shape[1], 1])
+    _ = sess.run(sflow_p,feed_dict={boundary_op: boundary_np})[0]
+    t = time.time()
+    for i in xrange(num_runs):
+      # make boundary
       # calc logits 
-      sflow_generated = sess.run(sflow_p,feed_dict={boundary_op: boundary_np})[0]
+      _ = sess.run(sflow_p,feed_dict={boundary_op: boundary_np})[0]
 
-      if FLAGS.display_test: 
-        # convert to display 
-        sflow_plot = np.concatenate([sflow_true, sflow_generated, sflow_true - sflow_generated], axis=1) 
-        boundary_concat = np.concatenate(3*[boundary_np], axis=2) 
-        sflow_plot = np.sqrt(np.square(sflow_plot[:,:,0]) + np.square(sflow_plot[:,:,1])) - .05 *boundary_concat[0,:,:,0]
-
-        # display it
-        plt.imshow(sflow_plot)
-        plt.colorbar()
-        plt.show()
-
-    print("the percent error on " + FLAGS.test_set + " is")
-    print(p_error)
+    elapsed = time.time() - t
+    print("time per inpu is ")
+    print(elapsed / (num_runs*8.))
 
 def main(argv=None):  # pylint: disable=unused-argument
   evaluate()
